ad412e55b07db96116253051944336c3a45c1ee3
nolanG <nolanrgiles@gmail.com>
2020-01-17 09:56:10 -0500

    NG[userRouter] created user router for user login registration routes

---
+++ quizlyapp/server/routes/userRouter.js
@@ -0,0 +1,61 @@
+const { Router } = require('express');
+const { User } = require('../models');
+const { hashPassword, genToken, checkPassword, restrict } = require('../services/auth');
+
+const userRouter = Router();
+
+const buildAuthResponse = (user) => {
+  const userData = {
+    username: user.username,
+    id: user.id
+  }
+
+  const token = genToken(userData);
+
+  return {
+    user: userData,
+    token
+  }
+}
+
+userRouter.post('/register', async (req, res, next) => { // check if user already exists on front-end do that before hitting this.
+  try {
+    const password_digest = await hashPassword(req.body.password);
+    const username = req.body.username; // const { username } = req.body;
+    const user = await User.create({
+      username,
+      password_digest
+    });
+
+    const respData = buildAuthResponse(user);
+    res.json(respData);
+  } catch (err) {
+    next(err);
+  }
+})
+
+userRouter.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username
+      }
+    })
+
+    if (await checkPassword(req.body.password, user.password_digest)) {
+      const respData = buildAuthResponse(user);
+      res.json(respData);
+    } else {
+      res.status(401).send('Invalid Credentials');
+    }
+  } catch (e) {
+    next(e);
+  }
+})
+
+userRouter.get('/verify', restrict, (req, res) => {
+  const user = res.locals.user;
+  res.json(user);
+})
+
+module.exports = userRouter;
