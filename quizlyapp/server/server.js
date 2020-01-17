b35ab5e0b8f523782fa46b17feb3e4fcf3a30397
nolanG <nolanrgiles@gmail.com>
2020-01-17 13:50:06 -0500

    added routerFile links to server.js file

--- quizlyapp/server/server.js
+++ quizlyapp/server/server.js
@@ -3,15 +3,15 @@ const PORT = process.env.PORT || 3003;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const userRouter = require('./routes/speakerRouter.js');
-const scoreRouter = require('./routes/quoteRouter.js')
+const userRouter = require('./routes/userRouter.js');
+const scoreRouter = require('./routes/scoreRouter.js')
 const triviaRouter = require('./routes/triviaRouter.js')
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/users', userRouter);
+app.use('/users/', userRouter);
 app.use('/score/', scoreRouter);
 app.use('/trivia/', triviaRouter); // I added this just to test to see if it would work for /quotes/:id -- it does but not how Casey had it setup
 
