aef19e2dc6bb339948faebf85d51c98698b77375
sinaraarliss <sinaramartins@gmail.com>
2020-01-17 15:48:40 -0500

    SA[App.js] Updated app.js trying to get login/register users to work

--- quizlyapp/client/src/components/Header.js
+++ quizlyapp/client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => (
-  <div>
-    <h4>Welcome to Quizly!</h4>
-  </div>
+  <header>
+    <h1>Welcome to Quizly!</h1>
+    <Link to='/' > Home </Link>
+  </header>
 )
 
 export default Header;
