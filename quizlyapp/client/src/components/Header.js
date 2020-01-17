418e08714135fb16f5f59ed9360625a40e7712f2
sinaraarliss <sinaramartins@gmail.com>
2020-01-17 12:05:24 -0500

    SA[Header] Created Header component and added boilerplate

---
+++ quizlyapp/client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => (
+  <div>
+    <Link to='/' > Home </Link>
+    <h2>Welcome to Quizly!</h2>
+  </div>
+)
+
+export default Home;
