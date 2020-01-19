6873c313984433106c523844b13f0b054274b33b
nolanG <nolanrgiles@gmail.com>
2020-01-19 15:17:08 -0500

    NG[adminlanding] created adminlanding logic to show unapproved questions

--- quizlyapp/client/src/components/AdminLanding.js
+++ quizlyapp/client/src/components/AdminLanding.js
@@ -1,12 +1,78 @@
 import React from 'react'
 
-import { }
+import { getUnapprovedTrivia } from '../services/api_helper'
 
-export default function AdminLanding(props) {
+export default class AdminLanding extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div>
+    this.state = {
+      unapprovedList: [],
+      numberUnapproved: 0
+    }
+  }
 
-    </div>
-  )
+  loadUnapprovedList = async () => {
+    const unapprovedList = await getUnapprovedTrivia();
+
+    this.setState({
+      unapprovedList,
+      numberUnapproved: unapprovedList.length
+    })
+  }
+
+  showUnapprovedList = () => {
+    this.setState({
+      showUnapprovedList: true
+    })
+  }
+
+  componentDidMount = async () => {
+    this.loadUnapprovedList();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Number of unapproved trivia questions: {this.state.numberUnapproved}
+        </div>
+        <div>
+          Load yet to be approved trivia?
+          <button onClick={this.showUnapprovedList}>Load</button>
+        </div>
+
+        {this.state.unapprovedList.length > 0 && this.state.showUnapprovedList &&
+          this.state.unapprovedList.map((question, key) => (
+            <div className="unapproved-question" key={key}>
+              <div>
+                Question: {question.question}
+              </div>
+              <div>
+                Answer: {question.answer}
+              </div>
+              <div>
+                Option1: {question.option1}
+              </div>
+              <div>
+                Option2: {question.option2}
+              </div>
+              <div>
+                Option3: {question.option3}
+              </div>
+              <div>
+                Value: {question.value}
+              </div>
+              <div>
+                Category: {question.category}
+              </div>
+              <div>
+                Approved?: {question.approved}
+              </div>
+            </div>
+          ))}
+      </div >
+    )
+  }
+
 }
