41aef93e0591f23fd6acd81fedee9e5bb42e2ce9
nolanG <nolanrgiles@gmail.com>
2020-01-21 17:09:01 -0500

    NG[guestlanding] Added ability for more categories

--- quizlyapp/client/src/components/GuestLanding.js
+++ quizlyapp/client/src/components/GuestLanding.js
@@ -6,10 +6,18 @@ class GuestLanding extends Component {
     super(props)
 
     this.state = {
-      //I actually think we can make this a non-class component unless other features are needed.
+      categories: []
     }
   }
 
+  componentDidMount = () => {
+    let tempCategoryList = this.props.categoryList.slice(0);
+    tempCategoryList.push({ category: 'All Categories' });
+    tempCategoryList.push({ category: 'Only Difficult Questions' });
+    this.setState({
+      categories: tempCategoryList
+    })
+  }
 
   //to replicate this for userboard you'll need both categoryList and setCategory from App.js
   render() {
@@ -17,11 +25,10 @@ class GuestLanding extends Component {
       <div>
         <h3>Click on any of the categories to enter the game:</h3>
         <div className="button-wrapper">
-          {this.props.categoryList.map((category, key) => ( //mapping through the array of categories ( which happen to be objects, so like {category: animals} )
+          {this.state.categories.map((category, key) => ( //mapping through the array of categories ( which happen to be objects, so like {category: animals} )
             <div key={key}>
               <Link to='/gameboard'>
                 <button className={`button${key + 1}`} onClick={(e) => { // giving each button a unique class for css
-                  // e.preventDefault();  It won't work with this, idk why.
                   this.props.setCategory(category.category) // this is a function in App.js that is passed to this one, we need to call this to set the category in App.js so GameBoard knows which category to load questions from
                 }
                 }>{category.category}</button>
